32f39cc4ebaf6e544baf5d62f98eeb96e24551e8
mhrah <mhrahatulislam@gmail.com>
2024-03-17 16:54:47 +0600

    commit 4:54

---
+++ app/src/main/java/com/example/retrofitapi/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.retrofitapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    public RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerView);
+
+
+    }
+}
